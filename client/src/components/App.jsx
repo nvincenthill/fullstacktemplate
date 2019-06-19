770aed5b16ded9140e8871790820c77ad1b24bca
elimoreh <elimoreh@berkeley.edu>
2019-06-18 22:19:11 -0700

    simplify template

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // consider Redux store, only use in certain situations
     this.state = {};
   }
 
   render() {
     return (
       <div>
-        <h1 className="title">MERN-Stack Template</h1>
-        <br />
-        <p>Happy coding!</p>
+        <p>hello</p>
       </div>
     );
   }
