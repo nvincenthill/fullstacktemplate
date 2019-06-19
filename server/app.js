0734eb5f061fa9a9d842b2cb26d86f84c533d4d1
elimoreh <elimoreh@berkeley.edu>
2019-06-18 21:15:03 -0700

    change port to 3000

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const routes = require('./../routes');
 
 const app = express();
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.static('public/'));
 app.use(express.static('client/dist'));
