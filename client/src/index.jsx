25827578e58e945744ac3aeee4d3e305e7027762
elimoreh <elimoreh@berkeley.edu>
2019-06-18 22:20:11 -0700

    provide app with redux store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './components/App';
+
 
 // Global styling
 import './styles/main.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
